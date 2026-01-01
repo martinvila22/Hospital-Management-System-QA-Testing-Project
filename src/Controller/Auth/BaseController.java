a391061891269b493cde96dd49d96c07849283da
martinvila22 <martin.vilabo@gmail.com>
2026-01-01 15:15:47 +0100

    SonarQube-BaseController-Change a method to match the overriding of a staticc variable & remove unused imports

--- src/Controller/Auth/BaseController.java
+++ src/Controller/Auth/BaseController.java
@@ -1,14 +1,9 @@
 package Controller.Auth;
 
-import Model.MYSQLDatabaseOp;
-import java.io.File;
-import java.io.FileWriter;
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -34,12 +29,14 @@ public class BaseController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         try {
-            staticAuthChangeView = authChangeView;
-            Parent login = FXMLLoader.load(getClass().getResource("/View/Auth/Login/Login.fxml"));
+            Parent login = FXMLLoader.load(
+                    getClass().getResource("/View/Auth/Login/Login.fxml")
+            );
             authChangeView.getChildren().setAll(login);
         } catch (IOException ex) {
-            System.out.println(ex);
+            ex.printStackTrace();
         }
     }
+
 
 }
