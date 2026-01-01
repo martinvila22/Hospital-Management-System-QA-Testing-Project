592f1362315c8fd97187d0ca2996e6a910386d7c
martinvila22 <martin.vilabo@gmail.com>
2026-01-01 13:44:29 +0100

    SonarQube-RemoveUserController-Removed unused imports & changed variable name to match regular expression

--- src/Controller/Admin/RemoveUser/RemoveUserController.java
+++ src/Controller/Admin/RemoveUser/RemoveUserController.java
@@ -8,16 +8,12 @@ import java.util.ResourceBundle;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;   // ✅ Import Scene
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class RemoveUserController implements Initializable {
@@ -50,9 +46,9 @@ public class RemoveUserController implements Initializable {
             return;
         }
 
-        String ID = selectedUser.getID();
+        String id = selectedUser.getID();
         try {
-            boolean deleteUserFlag = database.handleRemoveUser(Integer.parseInt(ID));
+            boolean deleteUserFlag = database.handleRemoveUser(Integer.parseInt(id));
             if (deleteUserFlag) {
                 loadUser();
                 Alert alert = new Alert(Alert.AlertType.INFORMATION, "Deleted Successfully", ButtonType.OK);
